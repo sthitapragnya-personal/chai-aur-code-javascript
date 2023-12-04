ac61d9740ab741bf8e7cf74e50ddf488e2c474cd
sthitapragnya-peronal <sthita635@gmail.com>
2023-12-05 03:21:37 +0530

    comparision continuing

---
+++ 04_comparision.js
@@ -0,0 +1,10 @@
+console.log(2>1);//true
+console.log(2<1);//false
+console.log(2>=1);//true
+console.log(2<=1);//flase
+console.log(2==1);//false
+console.log(2!=1);//true
+console.log(2==2);//true
+console.log(2===2);//true
+console.log("2">1);//true
+console.log("02">1);//true
