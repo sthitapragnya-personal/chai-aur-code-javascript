7ba255d579ad68261faa09956cab7aad825d6aa6
sthitapragnya-peronal <sthita635@gmail.com>
2023-12-08 11:25:46 +0530

    changes done

--- 04_comparision.js
+++ 04_comparision.js
@@ -1,10 +1,28 @@
-console.log(2>1);//true
-console.log(2<1);//false
-console.log(2>=1);//true
-console.log(2<=1);//flase
-console.log(2==1);//false
-console.log(2!=1);//true
-console.log(2==2);//true
-console.log(2===2);//true
-console.log("2">1);//true
-console.log("02">1);//true
+// console.log(2>1);//true
+// console.log(2<1);//false
+// console.log(2>=1);//true
+// console.log(2<=1);//flase
+// console.log(2==1);//false
+// console.log(2!=1);//true
+// console.log(2==2);//true
+// console.log(2===2);//true
+// console.log("2">1);//true
+// console.log("02">1);//true
+
+// console.log("a"=="b");//false
+
+// console.log(null>0);//false
+// console.log(null==0);//false
+// console.log(null>=0);//true
+// note : here in this case the  == and the >,<,>=,<= operators work differntly , >,<,>=,<= operators concert the null to number that is 0 but in case of == null is not converted to number 
+
+
+// and in case of undefined it is not changed to number in case of any operator 
+// console.log(undefined>0);//flase
+// console.log(undefined==0);//false
+// console.log(undefined>=0);//false
+
+
+// //== checks only value but === checks both value as well as datatype
+// console.log("2"==2);//true
+// console.log("2"===2);//false
