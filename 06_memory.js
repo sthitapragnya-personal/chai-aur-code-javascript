cc611419188dc7d74f29759972dc2e7f3f9cb1eb
sthitapragnya-peronal <sthita635@gmail.com>
2023-12-10 02:09:15 +0530

    done changes

---
+++ 06_memory.js
@@ -0,0 +1,23 @@
+//memory is of 2 types 
+// stack memory and heap memory
+
+// in case of primitive data types stack memory is used and un case of non- primitive datatype heap memory is used 
+
+// in case of stack memory the copy of the original memory /data is given and used but in case of heap memory the refence of the data/memory is passed and changes are made in the original data
+
+
+//concept of stack and refernce
+// let myyoutubename="sthita.youtube";
+// let anothername=myyoutubename;
+// console.log(myyoutubename+" "+anothername);
+// anothername="hello buddy";
+// console.log(myyoutubename+" "+anothername);
+
+// let userone={
+//     myname:"sthita",
+//     myage:21
+// }
+// let usertwo=userone;
+// console.log(userone+"  "+usertwo);
+// usertwo.myage=23
+// console.log(userone+"  "+usertwo);
---
+++ 07_strings.js
@@ -0,0 +1,138 @@
+//strings
+//----------
+// first go to console
+
+// console.log("hello"+"world");//helloworld
+// const name="sthita"
+// const repocount=50
+// console.log(name+repocount+"value");//sthita50value
+
+// //use of ``
+// //-----------
+// console.log(`hello my name is ${name} and my repocount is ${repocount}`);//hello my name is sthita and my repocount is 50
+
+// // another way to declare the String
+// // ------------------------------------
+// const game= new String("game1");
+// console.log(game); //[String: 'game1']
+// console.log(typeof game);//object
+
+
+// console.log(game[0]);//g
+
+
+// //string inbluid functions
+// //--------------------------
+// console.log(game.__proto__);//{}
+// console.log(game.length);//5 //used to get the length of the string
+// console.log(game.toUpperCase());//GAME1 //used to convert to uppercase
+// console.log(game.toLowerCase());//game1  //used to convert to lowercase
+// console.log(game.toLocaleUpperCase());//GAME1  //used to convert to uppercase
+// console.log(game.toLocaleLowerCase());//game1  //used to convert to lowercase
+// console.log(game.charAt(0));//g   //get the character at a given index
+// console.log(game.charAt(100));  //returns nothing if the index is invalid
+// console.log(game.indexOf('a'));//1  //used to get the index of a given character from first
+// console.log(game.indexOf('z'));//-1   //returns -1 if the character is not found
+
+
+
+// // comparing strings
+// //-------------------
+// const a = "a";
+// const b = "b";
+// if (a < b) {
+//   console.log(`${a} is less than ${b}`); //a is less than b
+// } else if (a > b) {
+//   console.log(`${a} is greater than ${b}`);
+// } else {
+//   console.log(`${a} and ${b} are equal.`);
+// }
+
+
+//string mdn js documentation starts from here
+//---------------------------------------------
+// the string can be sraeted as the string leterals or objects using the string() constructor
+// const string1 = "A string primitive";   //string literal
+// const string2 = 'Also a string primitive';   //string literal
+// const string3 = `Yet another string primitive`;   //string literal
+// console.log(typeof string1);//string   
+// console.log(typeof string1);//string   
+// console.log(typeof string1);//string   
+
+// const string4 = new String("A String object");// string object
+// console.log(typeof string4);//object
+
+// "cat".charAt(1); // gives value "a"
+// "cat"[1]; // gives value "a"
+
+// //comparing strings
+// //------------------
+// const a = "a";
+// const b = "b";
+// if (a < b) {
+//   // true
+//   console.log(`${a} is less than ${b}`);
+// } else if (a > b) {
+//   console.log(`${a} is greater than ${b}`);
+// } else {
+//   console.log(`${a} and ${b} are equal.`);
+// }
+
+// //string primitive/literal and string object
+// //--------------------------------------------
+// const strPrim = "foo"; // A literal is a string primitive
+// const strPrim2 = String(1); // Coerced into the string primitive "1"
+// const strPrim3 = String(true); // Coerced into the string primitive "true"
+// const strObj = new String(strPrim); // String with new returns a string wrapper object.
+
+// console.log(typeof strPrim); // "string"
+// console.log(typeof strPrim2); // "string"
+// console.log(typeof strPrim3); // "string"
+// console.log(typeof strObj); // "object"
+
+
+// //eval() and valueOf() function
+// //--------------------------------
+// //String primitives and String objects also give different results when using eval().
+// const s1 = "2 + 2"; // creates a string primitive
+// const s2 = new String("2 + 2"); // creates a String object
+// console.log(eval(s1)); // returns the number 4
+// console.log(eval(s2)); // returns the string "2 + 2"
+
+// // A String object can always be converted to its primitive counterpart with the valueOf() method.
+// console.log(eval(s2.valueOf())); // returns the number 4
+
+
+// //string methods
+// //---------------
+// // String.anchor() -> but yes this is depricated
+// //---------------------------------
+// const contentString = "Hello, world";
+// const x = contentString.anchor("hello"); //basically this converts a string to a anchor tag
+// console.log(x); //<a name="hello">Hello, world</a>
+
+// //String.prototype.at() -> used to get the element at a given index of the string and returns nothing for invalid index
+////----------------------------------------------------------
+// const x="sthita"
+// console.log(x.charAt(0));//s  //used to get the first element of the string
+// console.log(x.charAt(-1));//a //used to get the last element of the string
+// console.log(x.charAt(100));// returns nothing
+
+
+//String.prototype.big() -> depricated 
+//depricated ->String.prototype.blink()
+//String.prototype.bold()  ->depricated
+
+//String.prototype.charAt()
+//--------------------------
+// const game="game1"
+// console.log(game.charAt(0));//g   //get the character at a given index
+// console.log(game.charAt(100));  //returns nothing if the index is invalid
+
+
+//String.prototype.charCodeAt()
+//-----------------------------
+// const game="game1"
+// console.log(game.charCodeAt(0));//103   //get the assci value of the character at a given index
+// console.log(game.charCodeAt(100));//NaN  //returns NaN if the index is invalid
+
